0ee0e8e1be6148bbcc260b8af2f5391430f41fba
jedaqsaul <jedaqsaul@gmail.com>
2025-05-08 20:16:43 +0300

    creates and manipulates list

---
+++ lists.py
@@ -0,0 +1,30 @@
+# A list in python is an ordered collectoin of items
+# It can hold any type of data,
+# And is created using square brackets []
+from pprint import pp
+numbers=[1, 3, 455, 5]
+print(type(numbers))
+print(numbers)
+
+# You can also use the list() constructor
+empty =list()
+print(type(empty))
+print(empty)
+
+# Accessing elements by index (just like JS arrays)
+list_abc =['a', 'b', 'c', 'd']
+print(list_abc[0])
+print(list_abc[1])
+print(list_abc[2])
+print(list_abc[3])
+
+# List functions
+pp(dir(list_abc))
+
+print(len(list_abc))
+print(sorted([1, 3, 455, 5]))
+
+list_123=[1,2,3]
+print(list_123.pop()) #removes and returns the last element
+print(list_123.remove(1))
+print(list_123)
